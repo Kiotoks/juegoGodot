d0fe7c88268215b808e2965d04a833d875d0f7f1
kiotoks <nikogr2005@gmail.com>
2024-11-18 19:13:46 -0300

    Solucionados pequeños errores

--- scripts/character_body_3d.gd
+++ scripts/character_body_3d.gd
@@ -46,7 +46,7 @@ func _process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if numero_de_jugador <3:
+	if numero_de_jugador < 2:
 		if Input.is_action_just_pressed(teclaSaltar) and is_on_floor():
 			velocity.y = JUMP_VELOCITY
 		# Detecta las teclas de movimiento para determinar la dirección
